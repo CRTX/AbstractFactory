e88154faa98963c13134e76726d71239374bc923
CRTX <ruiz.d.christian@gmail.com>
2017-02-09 02:45:40 -0600

    Added travis support

---
+++ test/DummyAbstractClassFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the CRTX${0} package.
+ *
+ * (c) Christian Ruiz <ruiz.d.christian@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CRTX\AbstractFactory\Test;
+
+use CRTX\AbstractFactory\AbstractFactory;
+
+/**
+ * Stub class to test parent AbstractFactory class
+ *
+ * @author Christian Ruiz <ruiz.d.christian@gmail.com>
+ */
+class DummyAbstractClassFactory extends AbstractFactory
+{
+}
