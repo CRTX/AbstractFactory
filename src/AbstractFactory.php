de7a9ad73c5aca6c5fa26fa72a60717351e6da08
CRTX <ruiz.d.christian@gmail.com>
2017-02-08 18:13:54 -0600

    Added MIT license to code

--- src/AbstractFactory.php
+++ src/AbstractFactory.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the CRTX\AbstractFactory package.
+ *
+ * (c) Christian Ruiz <ruiz.d.christian@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace CRTX\AbstractFactory;
 
 use ReflectionClass;
